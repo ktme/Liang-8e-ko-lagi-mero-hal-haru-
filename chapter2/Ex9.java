8ad6699b66453170bed90bc1bc3d6706cac3e2c6
ktme <robin.joshi@selu.edu>
2014-07-02 11:17:19 -0500

    Financial application: monetary units

---
+++ chapter2/Ex9.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Ex9 {
+  public static void main(String[] args) {
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+
+    // Receive the amount
+    System.out.print(
+      "Enter an amount in cents, for instance 1156: ");
+    int remainingAmount = input.nextInt();
+	int amount = remainingAmount;
+	
+    // Find the number of one dollars
+    int numberOfOneDollars = remainingAmount / 100;
+    remainingAmount = remainingAmount % 100;
+
+    // Find the number of quarters in the remaining amount
+    int numberOfQuarters = remainingAmount / 25;
+    remainingAmount = remainingAmount % 25;
+
+    // Find the number of dimes in the remaining amount
+    int numberOfDimes = remainingAmount / 10;
+    remainingAmount = remainingAmount % 10;
+
+    // Find the number of nickels in the remaining amount
+    int numberOfNickels = remainingAmount / 5;
+    remainingAmount = remainingAmount % 5;
+
+    // Find the number of pennies in the remaining amount
+    int numberOfPennies = remainingAmount;
+
+    // Display results
+    String output = "Your amount " + amount + " consists of \n" +
+      "\t" + numberOfOneDollars + " dollars\n" +
+      "\t" + numberOfQuarters + " quarters\n" +
+      "\t" + numberOfDimes + " dimes\n" +
+      "\t" + numberOfNickels + " nickels\n" +
+      "\t" + numberOfPennies + " pennies";
+    System.out.println(output);
+  }
+}
