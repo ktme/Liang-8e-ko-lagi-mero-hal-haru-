90976f8f8fbd9dc4776ea4319e3a3e428d1f36cc
ktme <robin.joshi@selu.edu>
2014-07-02 11:26:27 -0500

    Using GUI input

---
+++ chapter2/Ex10.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane; 
+
+public class Ex10 {
+	public static void main(String[] args){
+		String amountString = JOptionPane.showInputDialog("Enter an amount in double");
+		double amount = Double.parseDouble(amountString);
+		
+		int remainingAmount = (int)(amount * 100);
+
+		// Find the number of one dollars
+		int numberOfOneDollars = remainingAmount / 100;
+		remainingAmount = remainingAmount % 100;
+
+		// Find the number of quarters in the remaining amount
+		int numberOfQuarters = remainingAmount / 25;
+		remainingAmount = remainingAmount % 25;
+
+		// Find the number of dimes in the remaining amount
+		int numberOfDimes = remainingAmount / 10;
+		remainingAmount = remainingAmount % 10;
+
+		// Find the number of nickels in the remaining amount
+		int numberOfNickels = remainingAmount / 5;
+		remainingAmount = remainingAmount % 5;
+
+		// Find the number of pennies in the remaining amount
+		int numberOfPennies = remainingAmount;
+
+		// Display results
+		String output = "Your amount " + amount + " consists of \n" +
+		"\t" + numberOfOneDollars + " dollars\n" +
+		"\t" + numberOfQuarters + " quarters\n" +
+		"\t" + numberOfDimes + " dimes\n" +
+		"\t" + numberOfNickels + " nickels\n" +
+		"\t" + numberOfPennies + " pennies";
+		
+		JOptionPane.showMessageDialog(null,output);
+	}
+}
