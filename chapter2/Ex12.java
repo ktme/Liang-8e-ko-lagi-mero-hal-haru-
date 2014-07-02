59effa662708e0d532c36af45f0c5d0afdf426f7
ktme <robin.joshi@selu.edu>
2014-07-02 18:32:42 -0500

    Financial applications: calculating interest)

---
+++ chapter2/Ex12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex12 {
+	public static void main(String[] args){
+		double balance, interestRate, interest; 
+		
+		System.out.println("Enter the balance and the interest rate: ");
+		Scanner input = new Scanner(System.in);
+		
+		balance = input.nextDouble();
+		interestRate = input.nextDouble();
+		
+		interest = balance * (interestRate/1200);
+		
+		
+		System.out.println("Interest is: "+interest);
+		
+	}
+}
