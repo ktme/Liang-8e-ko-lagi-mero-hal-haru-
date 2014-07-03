dee31cc688d44123dce14d8bff21c2c8beb3d8c6
ktme <ktme247@gmail.com>
2014-07-03 16:06:05 -0500

    Validating triangles

--- chapter3/Ex19.java
+++ chapter3/Ex19.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ex19 {
 	public static void main (String[] args){
-		double a, b, c;
+		double a, b, c; // a,b, and c represent the three sides of a triangle
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter the three sides of a triangle: " );
