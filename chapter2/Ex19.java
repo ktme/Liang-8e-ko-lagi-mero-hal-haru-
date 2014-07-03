238ce7af2f42cc454058886fc2e75f88485da3e2
ktme <robin.joshi@selu.edu>
2014-07-02 20:32:24 -0500

    Random Character

--- chapter2/Ex19.java
+++ chapter2/Ex19.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; 
 public class Ex19 {
 	public static void main(String[] args) {
-		System.out.println((char)(65 + System.currentTimeMillis()%27));
+		System.out.println((char)(65+System.currentTimeMillis()%27));
 	}
 }
 
