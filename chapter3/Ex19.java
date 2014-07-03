aed386d51a5e3d50ef90f67b2a08fbaa667ef3a4
ktme <ktme247@gmail.com>
2014-07-03 16:05:33 -0500

    Validating triangles

---
+++ chapter3/Ex19.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+
+public class Ex19 {
+	public static void main (String[] args){
+		double a, b, c;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the three sides of a triangle: " );
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();
+		
+		
+
+		if ((a>b+c) || (b>a+c) || (c>a+b))
+			System.out.println("Triangle is not possible.");
+		else 
+			System.out.println("Triangle is possible!");
+	}
+
+}
