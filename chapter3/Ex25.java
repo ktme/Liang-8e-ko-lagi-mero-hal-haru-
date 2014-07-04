660e1e95e84328c2669218190cdfe6599892d96b
ktme <ktme247@gmail.com>
2014-07-03 21:34:24 -0500

    Computing the perimeter of a triangle

---
+++ chapter3/Ex25.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+
+public class Ex25{
+	public static void main (String[] args){
+		double a, b, c, p; // a,b, and c represent the three sides of a triangle
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the three sides of a triangle: " );
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();
+
+
+
+		if ((a>b+c) || (b>a+c) || (c>a+b))
+			System.out.println("Triangle is not possible.");
+		else {
+			p = a+b+c;
+			System.out.println("Perimeter: "+p);
+		}
+	}
+
+}
