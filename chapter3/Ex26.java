09efae242948e18a4a4cefd7c0e5ac5804f1ce6f
ktme <ktme247@gmail.com>
2014-07-03 21:57:41 -0500

    Using the &&, || and ^ operators

---
+++ chapter3/Ex26.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+public class Ex26 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		boolean value = false;
+		System.out.println("Enter an integer: ");
+		int num = input.nextInt();
+		value = ((num%5 == 0 ) && (num%6 ==0));
+		System.out.println(num+" is divisible by both 5 and 6:" + value);
+		value = ((num %5 == 0) || (num%6 == 0));
+		System.out.println(num+" is divisible by either 5 or 6:"+ value);
+		value = ((num %5 == 0) || (num%6 == 0)) && !((num%5 == 0 ) && (num%6 ==0));
+		System.out.println(num+" is divisible by either 5 or 6, but not both:"+ value);
+	}
+}
