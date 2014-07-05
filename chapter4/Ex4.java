d0e249e0314e52e95c88d6a9f70768c1d21d5462
ktme <ktme247@gmail.com>
2014-07-05 04:25:17 -0500

    Conversion from miles to kilometers

---
+++ chapter4/Ex4.java
@@ -0,0 +1,13 @@
+public class Ex4 {
+	public static void main(String[] args){
+		double kms;
+		int miles = 1; 
+		System.out.println("miles\t\tkilometers");
+		
+		while (miles <= 199) {
+			kms = miles*1.609;
+			System.out.println(miles + "\t\t" + kms);
+			miles++;
+		}
+	}
+}
