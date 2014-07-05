e0411f3129ce322015869e2a36d9d6ce61f349ee
ktme <ktme247@gmail.com>
2014-07-05 10:00:43 -0500

    Finding numbers divisible by 5 AND 6

---
+++ chapter4/Ex10.java
@@ -0,0 +1,12 @@
+public class Ex10 {
+	public static void main(String [] args){
+		for (int i = 100,count = 0; i <= 1000; i++){
+			if ((i%5==0) && (i%6==0)){
+				count++;
+				System.out.print(i+" ");
+				if(count%10==0) System.out.println();
+			}
+				
+		}
+	}
+}
