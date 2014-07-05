5cde6fa7936700661a954a02466e986c856e84fd
Thinking Buddha <ktme247@gmail.com>
2014-07-05 10:43:10 -0500

    Update Ex15.java

--- chapter4/Ex15.java
+++ chapter4/Ex15.java
@@ -10,9 +10,7 @@ public class Ex15 {
 		
 		if(n1<n2) d = n1;
 		else if (n1>n2) d = n2; 
-		else{
-			gcd = n1;
-		}
+		else gcd = n1;
 		while (d >= 1 && gcd !=n1) {
 			if((n1%d==0) && (n2%d==0)) {
 				gcd = d;
