825ff4f02d02dfc5587fa5975a9165d320dcab62
ktme <ktme247@gmail.com>
2014-07-07 12:45:48 -0500

    Summation

---
+++ chapter4/Ex35.java
@@ -0,0 +1,11 @@
+public class Ex35 {
+	public static void main(String[] args){
+		int i;
+		double sum = 0.0d; 
+		
+		for (i = 1; i<=624; i++) {
+			sum += 1.0d/(Math.sqrt(i)+Math.sqrt(i+1)) ;
+		}
+		System.out.println("Sum: "+sum);
+	}
+}
