0aed441dfa2c86fc65183cbd579e6077a8c64506
ktme <ktme247@gmail.com>
2014-07-08 12:35:54 -0500

    Computing a Series

---
+++ chapter5/Ex13.java
@@ -0,0 +1,17 @@
+public class Ex13{
+	public static void main(String [] args) {
+		System.out.println("i\t\t\tm(i)");
+		
+		for (int i = 1; i <= 20; i++) 	
+			System.out.printf("%d\t\t\t%6.4f\n",i,m(i));
+		
+	}
+	public static double m(int i) {
+		double sum = 0;
+		
+		for (int j = 1; j <= i; j++ ){
+			sum+= (double)j/(j+1);
+		}
+		return sum;
+	}
+} 
