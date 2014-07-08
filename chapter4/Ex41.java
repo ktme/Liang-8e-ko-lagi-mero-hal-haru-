641ec20e14168eba239d9a8009c429406b68831c
Thinking Buddha <ktme247@gmail.com>
2014-07-07 19:26:02 -0500

    Update Ex41.java

--- chapter4/Ex41.java
+++ chapter4/Ex41.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Ex41{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		int max = 0, count = 0;
-		int number ;
+		int max = 0, count = 0;                  // maximum and frequency of maximum
+		int number ;                   
 		
 		while ((number = input.nextInt()) != 0){
 			if (number > max){
 				max = number; 
-				count = 1;
+				count = 1;               // set count to 1 when a new max is found
 			}
 			else if (number == max) {
-				count++;
+				count++;                // if there's another max, increment count
 			}
 		}
 		
