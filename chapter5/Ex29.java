36bbcf4e8ed225b8ad8508f6f07c94961a33188c
ktme <ktme247@gmail.com>
2014-07-09 21:06:35 -0500

    Game: craps

---
+++ chapter5/Ex29.java
@@ -0,0 +1,33 @@
+public class Ex29 {
+	public static void main (String[] args) {
+		  
+		int sum = dieSum();
+		int point;
+		
+		if(sum == 2 || sum == 3 || sum == 12)
+			System.out.println("You loose.");
+		else if (sum == 7 || sum == 11)
+			System.out.println("You win.");
+		else {
+			System.out.println("You are awarded a point.");
+			
+			point = dieSum();
+			System.out.println("point is "+point);
+			sum = dieSum();
+			
+			if (sum == 7) System.out.println("You loose.");
+			else if(sum == point) {
+				System.out.println("You win.");
+			}
+			else System.out.println("You loose.");
+		}
+	}
+	public static int dieSum(){
+		int die1 = (int)(Math.random()*6)+ 1;
+		int die2 = (int)(Math.random()*6)+ 1;
+		int sum = die1+die2;
+		System.out.println("You rolled "+die1+" + " + die2+ " = " + sum);
+		
+		return sum;
+	}
+}
