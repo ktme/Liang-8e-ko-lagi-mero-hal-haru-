9da801f78d994601a4871e3dd01623e1fb84781c
Thinking Buddha <ktme247@gmail.com>
2014-07-10 14:10:17 -0500

    Update Ex5.java

--- chapter5/Ex5.java
+++ chapter5/Ex5.java
@@ -19,7 +19,7 @@ public class Ex5 {
 			if (num2<num3) System.out.print(num2+" < "+num3);
 			else System.out.print(num3+" < "+num2);
 		}  
-		else if(num2<num1 && num2< num3){
+		else if(num2< num3){
 			System.out.print(num2+" < ");
 			if(num1<num3) System.out.print(num1+" < "+num3);
 			else System.out.print(num3+" < "+num1);
